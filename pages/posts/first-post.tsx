e9b0445854338d45769fe5744fedb105b76a9644
mkchauhan647 <mkchauhan647@gmail.com>
2022-12-10 15:18:39 +0545

    .

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -7,7 +7,7 @@ import Layout from "../../components/Layout";
 export default function FirstPost() {
     return (
       <>
-      <Layout>
+      <Layout home={false}>
         <Head>
           <title>First Post</title>
         </Head>
